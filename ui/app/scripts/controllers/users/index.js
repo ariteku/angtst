6e596d3261ea81a752262aeb3a7bbf1191240c05
大城 一真 <oshiro_kazuma@ca-adv.co.jp>
2014-08-13 19:50:56 +0900

    ユーザー追加 UI実装

--- ui/app/scripts/controllers/users/index.js
+++ ui/app/scripts/controllers/users/index.js
@@ -1,5 +1,17 @@
-angular.module('uiApp').controller('UserIndexCtrl', ['$scope', 'User', function ($scope, User) {
+angular.module('uiApp').controller('UserIndexCtrl', ['$scope', '$http', 'User', function ($scope, $http, User) {
   'use strict';
 
   $scope.users = User.query();
+
+  $scope.add = function () {
+    $http({method: "GET", url: "/api/user/add"}).
+      success(function(data, status) {
+        console.log("追加っす！");
+        $scope.users = User.query();
+      }).
+      error(function(data, status) {
+        alert("失敗＞＜");
+      });
+  };
+
 }]);
